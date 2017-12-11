9ede129ec53c8933a6aac0fa552653ef834831fc
bastronaut <btsijtsma@gmail.com>
2017-12-11 22:45:12 +0100

    verified code, pre parameter optimization

--- trader/trader.py
+++ trader/trader.py
@@ -7,13 +7,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument("k", help="api key")
 parser.add_argument("s", help="api secret")
 import time
+import datetime
 
 CURRENCYPAIR = "USDT_LTC"
 ONEWEEKSECONDS = 604800
 NROFWEEKS = 8
-DROPVALUE = 0.90
-GAINVALUE = 1.03
-BACKUPGAINVALUE = 1.01 # value that, if crossed by this value over most recente salesprice, will be bought in for again
+DROPVALUE = 0.93
+GAINVALUE = 1.04
+BACKUPGAINVALUE = 1.025 # value that, if crossed by this value over most recente salesprice, will be bought in for again
 TRADINGFEE = 0.0025 # poloniex worst case fee
 
 def main():
@@ -33,72 +34,132 @@ def backtest(db):
         ltclist.append(i)
     print len(ltclist)
 
-    anchorprice = ltclist[0]['weightedAverage']
+    curprice = ltclist[0]['weightedAverage']
 
-    lowestprice = anchorprice
-    saleprice = anchorprice * DROPVALUE # init, at this price, sell if holding
-    increase = saleprice * GAINVALUE # at this price, buy in again
-    backupincrease = saleprice * BACKUPGAINVALUE # at this price, buy in again if price went up over sales price
-
+    lowestprice = curprice
+    topprice = curprice
+    sellprice = curprice * DROPVALUE # init, at this price, sell if holding
+    buybottom = sellprice * GAINVALUE # at this price, buy in again
+    buytop = sellprice * BACKUPGAINVALUE # at this price, buy in again - price in case buy bottom is never touched
     sidelined = False
     usd = 0
     ltcqty = 1
     feespaid = []
     print "starting LTC amount: {}, starting USD amount: {}".format(ltcqty, usd)
-    print "anchorprice: {}, saleprice: {}, increase: {}, backupincrease: {}".format(anchorprice, saleprice, increase, backupincrease)
+    print "curprice: {}, sellprice: {}, buybottom: {}, buytop: {}".format(curprice, sellprice, buybottom, buytop)
 
     for i in ltclist:
         curprice = i['weightedAverage']
 
         if curprice < lowestprice:
             lowestprice = curprice
-            increase = lowestprice * GAINVALUE
+            buybottom = lowestprice * GAINVALUE
 
-        if curprice > anchorprice:
-            anchorprice = curprice
-            saleprice = anchorprice * DROPVALUE
+        if curprice > topprice:
+            topprice = curprice
+            sellprice = topprice * DROPVALUE
 
         if sidelined:
-            if curprice > backupincrease:
-                '''Price over the top buy boundary, buy in again'''
+            if curprice > buytop:
+                # do buy
                 ltcqty = (usd / curprice) * (1-TRADINGFEE)
                 fee = usd * TRADINGFEE
-                feespaid.append(fee)
-                print "bought {} at price {} for fee {} at backupprice".format(ltcqty, curprice, fee)
                 usd = 0
+                feespaid.append(fee)
+                dateepoch = i['date']
+                date = datetime.datetime.fromtimestamp(dateepoch).strftime('%Y-%m-%d %H:%M:%S')
+                print "bought {} at price {} for fee {} at backupprice, on date {}\n".format(ltcqty, curprice, fee, date)
+
                 sidelined = False
-                anchorprice = curprice
-                saleprice = anchorprice * DROPVALUE
-                increase = saleprice * GAINVALUE
+                topprice = curprice
+                sellprice = topprice * DROPVALUE
 
-            elif curprice > increase:
-                '''Price over the bottom buy boundary, buy in again'''
-
+            elif curprice > buybottom:
+                # do buy
                 ltcqty = (usd / curprice) * (1-TRADINGFEE)
                 fee = usd * TRADINGFEE
-                feespaid.append(fee)
-                print "bought {} at price {} for fee {} at increase price".format(ltcqty, curprice, fee)
                 usd = 0
+                feespaid.append(fee)
+                dateepoch = i['date']
+                date = datetime.datetime.fromtimestamp(dateepoch).strftime('%Y-%m-%d %H:%M:%S')
+                print "bought {} at price {} for fee {} at backupprice, on date {}\n".format(ltcqty, curprice, fee, date)
+
                 sidelined = False
-                anchorprice = curprice
-                saleprice = anchorprice * DROPVALUE
-                increase = saleprice * GAINVALUE
-
-
-
-
+                topprice = curprice
+                sellprice = topprice * DROPVALUE
 
         if not sidelined:
-            if curprice < saleprice:
-                '''Price below sell threshold, sell'''
+            if curprice < sellprice:
+                # do sell
                 usd = (ltcqty * curprice) * (1-TRADINGFEE)
                 fee = (ltcqty * curprice) * TRADINGFEE
                 feespaid.append(fee)
-                print "sold {} at price {} for fee {}".format(ltcqty, curprice, fee)
+                dateepoch = i['date']
+                date = datetime.datetime.fromtimestamp(dateepoch).strftime('%Y-%m-%d %H:%M:%S')
+                print "sold {} at price {} for fee {} on date {}\n".format(ltcqty, curprice, fee, date)
+
                 ltcqty = 0
-                lowestprice = curprice
+
                 sidelined = True
-                backupincrease = curprice * BACKUPGAINVALUE
+                buytop = curprice * BACKUPGAINVALUE
+                buybottom = curprice * GAINVALUE
+                lowestprice = curprice
+
+
+
+
+
+        #
+        # if curprice < lowestprice:
+        #     lowestprice = curprice
+        #     buybottom =
+        #
+        # if curprice > anchorprice:
+        #     anchorprice = curprice
+        #     saleprice = anchorprice * DROPVALUE
+        #
+        # if sidelined:
+        #     if curprice > backupincrease:
+        #         '''Price over the top buy boundary, buy in again'''
+        #         ltcqty = (usd / curprice) * (1-TRADINGFEE)
+        #         fee = usd * TRADINGFEE
+        #         feespaid.append(fee)
+        #         print "bought {} at price {} for fee {} at backupprice".format(ltcqty, curprice, fee)
+        #         usd = 0
+        #         sidelined = False
+        #         anchorprice = curprice
+        #         saleprice = anchorprice * DROPVALUE
+        #         increase = saleprice * GAINVALUE
+        #
+        #     elif curprice > increase:
+        #         '''Price over the bottom buy boundary, buy in again'''
+        #
+        #         ltcqty = (usd / curprice) * (1-TRADINGFEE)
+        #         fee = usd * TRADINGFEE
+        #         feespaid.append(fee)
+        #         print "bought {} at price {} for fee {} at increase price".format(ltcqty, curprice, fee)
+        #         usd = 0
+        #         sidelined = False
+        #         anchorprice = curprice
+        #         saleprice = anchorprice * DROPVALUE
+        #         increase = saleprice * GAINVALUE
+        #
+        #
+        #
+        #
+        #
+        # if not sidelined:
+        #     if curprice < saleprice:
+        #         '''Price below sell threshold, sell'''
+        #         usd = (ltcqty * curprice) * (1-TRADINGFEE)
+        #         fee = (ltcqty * curprice) * TRADINGFEE
+        #         feespaid.append(fee)
+        #         print "sold {} at price {} for fee {}".format(ltcqty, curprice, fee)
+        #         ltcqty = 0
+        #         lowestprice = curprice
+        #         sidelined = True
+        #         backupincrease = curprice * BACKUPGAINVALUE
+
 
 
     print "Resulting ltc: " + str(ltcqty)
